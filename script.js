de6560fb7caac310ef2498b0ee95bfda76ec3d31
Mari Tatarchuk <mashatatarchuk@yandex.ru>
2021-04-05 22:05:25 +0400

    did a task

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-alert('Hello!');
+let money = 10000 ;
+let income = 'rent';
+let addExpenses = 'Manicure,shopping,coffee';
+let deposit = true;
+let mission = 20000;
+let period = 6 ;
 
-console.log('123');
+console.log(typeof money);
 
-let money = '10000';
-let income = '5000';
-let addExpenses = '4000';
-let deposit = '1000';
-let mission = 'win';
-let period = '2 month';
+console.log(typeof income);
 
-console.log(money, income, addExpenses, deposit, mission, period );
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+let phrase1 = `Период равен ${period} месяцев`;
+console.log(phrase1);
+
+let phrase2 = `Цель заработать ${mission} долларов`;
+console.log(phrase2);
+
+console.log(addExpenses.toLowerCase());
+
+console.log(addExpenses.split(','));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+
+
+
+
+
+
