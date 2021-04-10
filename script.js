a291b20485204a57805acac4bacf18e4e6d9f5b9
Mari Tatarchuk <mashatatarchuk@yandex.ru>
2021-04-10 17:44:57 +0400

    if was edited

--- script.js
+++ script.js
@@ -42,12 +42,12 @@ let budgetDay = Math.floor(budgetMonth/30);
 
 console.log('Бюджет на день ' + budgetDay);
 
-if(budgetDay >= 1200){
+if(budgetDay > 1200){
   console.log('У вас высокий уровень дохода');
-}else if(budgetDay >= 600){
+}else if(budgetDay >= 600 && budgetDay <= 1200){
   console.log('У вас средний уровень дохода');
-} else if(budgetDay >= 0){
+} else if(budgetDay >= 0 && budgetDay < 600){
   console.log('К сожалению у вас уровень дохода ниже среднего')
-} else if(budgetDay < 0){
+} else {
   console.log('Что то пошло не так');
 }
